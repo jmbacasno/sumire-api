7c71883c184a2edb5dc970d20854c33f47eb7d73
John Michael <bacasno.johnmichael@gmail.com>
2026-02-21 21:43:26 +0800

    feat: remove unused domain enums

--- src/domain/enums.py
+++ src/domain/enums.py
@@ -1,15 +1,7 @@
 from enum import IntEnum
 
-class RepeatType(IntEnum):
-    DAILY = 1
-    WEEKDAYS = "WEEDAYS"
-    WEEKLY = "WEELY"
-    MONTHLY = 4
-    YEARLY = 5
-    CUSTOM = 6
-
-class RepeatInterval(IntEnum):
-    DAYS = 1
-    WEEKS = 2
-    MONTHS = 3
-    YEARS = 4
+class RepeatFrequency(IntEnum):
+    YEARLY = 0
+    MONTHLY = 1
+    WEEKLY = 2
+    DAILY = 3
