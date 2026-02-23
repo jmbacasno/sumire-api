d73c6037dea77499fb87deb60f4eb12bae310ebd
John Michael <bacasno.johnmichael@gmail.com>
2026-02-23 15:50:39 +0800

    reafctor: change implementation to normal classes instead of dataclasses to prevent overengineering

--- src/domain/entities_new.py
+++ src/domain/entities_new.py
@@ -18,26 +18,25 @@ class Frequency(IntEnum):
     WEEKLY = 2
     DAILY = 3
 
-@dataclass
 class Repeat:
-    frequency: Frequency
-    interval: int
-
-    def __post_init__(self):
-        if self.frequency is None:
+    def __init__(self, frequency: Frequency, interval: int):
+        if frequency is None:
             raise InvalidEntityStateException(
                 "Frequency cannot be empty. Repeat must have a frequency."
             )
 
-        if self.interval is None:
+        if interval is None:
             raise InvalidEntityStateException(
                 "Interval cannot be empty. Repeat must have an interval."
             )
 
-        if self.interval < 1:
+        if interval < 1:
             raise InvalidEntityStateException(
-                "Interval must be a positive integer."
+                f"Invalid interval: {interval}. Interval must be a positive integer."
             )
+
+        self.frequency = frequency
+        self.interval = interval
 
     def get_next_date(self, reference_date: datetime) -> datetime:
         """Get next date."""
@@ -49,61 +48,52 @@ class Repeat:
         )
         return repeat_rrule.after(reference_date)
 
-@dataclass
 class YearlyRepeat(Repeat):
-    frequency: Frequency = field(default=Frequency.YEARLY, init=False)
+    def __init__(self, interval: int):
+        super().__init__(Frequency.YEARLY, interval)
 
     def __str__(self):
         if self.interval == 1:
             return "Repeat yearly"
         return f"Repeat every {self.interval} years"
 
-@dataclass
 class MonthlyRepeat(Repeat):
-    frequency: Frequency = field(default=Frequency.MONTHLY, init=False)
+    def __init__(self, interval: int):
+        super().__init__(Frequency.MONTHLY, interval)
 
     def __str__(self):
         if self.interval == 1:
             return "Repeat monthly"
         return f"Repeat every {self.interval} months"
 
-@dataclass
 class WeeklyRepeat(Repeat):
-    frequency: Frequency = field(default=Frequency.WEEKLY, init=False)
-    allowed_weekdays: Set[int] | str
-
-    def __post_init__(self):
-        super().__post_init__()
-        if self.allowed_weekdays is None or len(self.allowed_weekdays) == 0:
+    def __init__(self, interval: int, allowed_weekdays: Set[int] | str):
+        super().__init__(Frequency.WEEKLY, interval)
+        if allowed_weekdays is None or len(allowed_weekdays) == 0:
             raise InvalidEntityStateException(
-                "Allowed weekdays cannot be empty. Weekly repeat must have allowed weekdays."
+                "Allowed weekdays cannot be empty. Repeat must have allowed weekdays."
             )
 
-        # Convert string to set
-        if isinstance(self.allowed_weekdays, str):
-            try:
-                self.allowed_weekdays = set(map(int, self.allowed_weekdays))
-            except ValueError:
+        for weekday in allowed_weekdays:
+            if weekday not in "0123456":
                 raise InvalidEntityStateException(
-                    "Cannot convert string to set of integers. Allowed weekdays must contain only integers."
+                    f"Invalid allowed weekdays: {allowed_weekdays}. Allowed weekdays must be string of integers from 0 (Monday) to 6 (Sunday)."
                 )
 
-        # Check if all weekdays are valid
-        for weekday in self.allowed_weekdays:
-            if weekday < 0 or weekday > 6:
-                raise InvalidEntityStateException(
-                    "Allowed weekdays must be from 0 (Monday) to 6 (Sunday)."
-                )
+        # Get a set of allowed weekdays from string
+        self.allowed_weekdays_set = set(map(int, allowed_weekdays))
+        # Get a clean string of allowed weekdays from set
+        self.allowed_weekdays = "".join(map(str, self.allowed_weekdays_set))
 
     def __str__(self):
         repeat_description = "Repeat weekly" if self.interval == 1 else f"Repeat every {self.interval} weeks"
-        
+
         if self.allowed_weekdays == WEEKDAY_WEEKDAYS:
             weekdays_description = "weekdays"
         elif self.allowed_weekdays == WEEKEND_WEEKDAYS:
             weekdays_description = "weekends"
         else:
-            weekdays_description = ", ".join(calendar.day_abbr[weekday] for weekday in self.allowed_weekdays)
+            weekdays_description = ", ".join(calendar.day_abbr[weekday] for weekday in self.allowed_weekdays_set)
 
         return f"{repeat_description} on {weekdays_description}"
 
@@ -114,13 +104,13 @@ class WeeklyRepeat(Repeat):
             freq=self.frequency,
             interval=self.interval,
             wkst=START_WEEKDAY,
-            byweekday=self.allowed_weekdays
+            byweekday=self.allowed_weekdays_set
         )
         return weekly_repeat_rrule.after(reference_date)
 
-@dataclass
 class DailyRepeat(Repeat):
-    frequency: Frequency = field(default=Frequency.DAILY, init=False)
+    def __init__(self, interval: int):
+        super().__init__(Frequency.DAILY, interval)
 
     def __str__(self):
         if self.interval == 1:
@@ -149,4 +139,4 @@ class RepeatFactory:
             case Frequency.DAILY:
                 return DailyRepeat(interval=interval)
             case _:
-                raise BusinessRuleViolationException(f"Frequency must be from 0 (Yearly) to 3 (Daily).")
+                raise InvalidEntityStateException(f"Frequency must be from 0 (Yearly) to 3 (Daily).")
