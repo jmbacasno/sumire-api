aadcbff7c4593db08c78333c04b4557f316709ed
John Michael <bacasno.johnmichael@gmail.com>
2026-03-01 15:44:19 +0800

    feat: delete unused domain modules

--- src/domain/constants.py
+++
@@ -1,5 +0,0 @@
-from typing import Set
-
-START_WEEKDAY: int = 6 # Sunday
-WEEKDAY_WEEKDAYS: Set[int] = {0, 1, 2, 3, 4} # Monday to Friday
-WEEKEND_WEEKDAYS: Set[int] = {5, 6} # Saturday and Sunday
--- src/domain/enums.py
+++
@@ -1,7 +0,0 @@
-from enum import IntEnum
-
-class RepeatFrequency(IntEnum):
-    YEARLY = 0
-    MONTHLY = 1
-    WEEKLY = 2
-    DAILY = 3
